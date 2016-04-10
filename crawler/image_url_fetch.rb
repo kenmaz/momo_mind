facb24a3109e788854144124f9bfbe3497db9d5c
Kentaro Matsumae <kentaro.matsumae@dena.com>
2016-04-10 16:03:40 +0900

    fix

---
+++ crawler/image_url_fetch.rb
@@ -0,0 +1,45 @@
+require 'net/http'
+require 'json'
+require 'open-uri'
+require 'digest/md5'
+
+#
+# keywordでbing画像検索し画像のURLを取得しcsvを出力します
+# csv:
+#  <keyword>,<url>,<url-md5_hash>
+#
+
+APIKEY = 'o1W2+VEhae2vI5OLW6pXpL5b2sd2+D7w9pTLOb0eyFk'
+
+def get(keyword, skip = 0)
+  url = "https://api.datamarket.azure.com/Bing/Search/v1/Composite?Sources=%27image%27&Query=#{query(keyword)}&Market=%27ja-JP%27&Adult=%27Strict%27&ImageFilters=%27Size%3AMedium%27&$format=json&$skip=#{skip}"
+
+  uri = URI(url)
+  req = Net::HTTP::Get.new(uri.request_uri)
+  req.basic_auth('', APIKEY)
+  res = Net::HTTP.start(uri.hostname, uri.port, :use_ssl => uri.scheme == 'https') do |http|
+    http.request(req)
+  end
+  body = JSON.parse(res.body, :symbolize_names => true)
+  body[:d][:results][0][:Image].each do |page|
+    media_url = page[:MediaUrl]
+    md5 = Digest::MD5.hexdigest(media_url)
+    puts "#{keyword},#{media_url},#{md5}"
+  end
+
+  sleep 5
+end
+
+def query(search_term)
+  return URI.encode_www_form_component('\'' + search_term + '\'')
+end
+
+get('ももクロ')
+get('ももいろクローバーZ')
+get('百田夏菜子')
+get('玉井詩織')
+get('佐々木彩夏')
+get('有安杏果')
+get('高城れに')
+get('川上アキラ')
+get('古屋智美')
