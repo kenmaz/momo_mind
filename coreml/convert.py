2fa90b1d81d6d0db8a9cc96b726835d1195f56aa
Kentaro Matsumae <kentaro.matsumae@dena.com>
2017-06-13 23:29:28 +0900

    fix

--- coreml/convert.py
+++ coreml/convert.py
@@ -1,8 +1,10 @@
-#from keras.models import load_model
 path = '/Users/kentaro.matsumae/Projects/momo_mind/keras/model.h5'
-#model = load_model(path)
 
 import coremltools
-coreml_model = coremltools.converters.keras.convert(path, input_names = 'image', image_input_names = 'image', class_labels = 'labels.txt')
-coreml_model.save('cifar10.mlmodel')
+coreml_model = coremltools.converters.keras.convert(path,
+        input_names = 'image',
+        image_input_names = 'image',
+        class_labels = 'labels.txt')
+
+coreml_model.save('Cifar10.mlmodel')
 
