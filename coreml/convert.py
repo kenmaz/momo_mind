10616a57781e2c81d12a3c6ab2fff69c6e073266
kenmaz <kentaro.matsumae@gmail.com>
2017-08-27 09:49:33 +0900

    xcode9 beta6 では不要

--- coreml/convert.py
+++ coreml/convert.py
@@ -4,7 +4,7 @@ import coremltools
 coreml_model = coremltools.converters.keras.convert(path,
         input_names = 'image',
         image_input_names = 'image',
-        is_bgr = True,
+        #is_bgr = True,
         image_scale = 0.00392156863,
         class_labels = 'labels.txt')
 
