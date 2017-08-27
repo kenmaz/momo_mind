7c8a853bc5d0f7c04cdc0d7d5a6b1f96852f8544
kenmaz <kentaro.matsumae@gmail.com>
2017-08-27 09:51:24 +0900

    refactor

--- keras/mcz_predict.py
+++ keras/mcz_predict.py
@@ -10,36 +10,28 @@ import mcz_input
 import sys
 import numpy as np
 
-(X_test, y_test)= mcz_input.read_data('../deeplearning/predict.txt')
-#(X_test, y_test)= mcz_input.read_data('../deeplearning/train.txt')
+def predict(input_path):
+    (X_test, y_test)= mcz_input.read_data(input_path)
 
-print(X_test)
+    X_test = X_test.astype('float32')
+    X_test /= 255
 
-X_test = X_test.astype('float32')
-X_test /= 255
+    nb_classes = 5
+    Y_test = np_utils.to_categorical(y_test, nb_classes)
 
-nb_classes = 5
-Y_test = np_utils.to_categorical(y_test, nb_classes)
-print(y_test)
-print(Y_test)
+    model = load_model('model.h5')
+    res = model.predict(X_test)
 
-model = load_model('model.h5')
-res = model.predict(X_test)
+    acts = [np.argmax(i) for i in res]
+    exps = [np.argmax(i) for i in Y_test]
+    cnt = 0.0
+    total = 0.0
+    for (a,e) in zip(acts, exps):
+        if a == e:
+            cnt += 1.0
+        total += 1.0
 
-print('res',res)
-print('Y_test',Y_test)
+    print(input_path, cnt/total)
 
-acts = [np.argmax(i) for i in res]
-exps = [np.argmax(i) for i in Y_test]
-
-cnt = 0
-total = 0
-
-for (a,e) in zip(acts, exps):
-    print(a,e)
-    if a == e:
-        cnt += 1
-    total += 1
-
-print(cnt/total)
-
+predict('../deeplearning/train.txt')
+predict('../deeplearning/test.txt')
